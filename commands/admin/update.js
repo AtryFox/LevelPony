d0f0ddba0f6cd88ec55a8e82afeb97848d371cc4
Benjamin Leuckefeld <code@deratrox.de>
2018-08-18 13:49:23 +0200

    adde update cmd

---
+++ commands/admin/update.js
@@ -0,0 +1,23 @@
+const {Command} = require('discord.js-commando');
+
+module.exports = class UpdateCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'update',
+            group: 'admin',
+            memberName: 'update',
+            description: 'Updates all user reward roles on this server. Fixes missing roles.',
+            examples: ['update'],
+            guildOnly: true,
+            userPermissions: ['ADMINISTRATOR']
+        });
+    }
+
+    async run(message, args) {
+        const members = message.guild.members;
+
+        members.forEach(member => {
+            message.client.levels.updateUserRoles(member, message);
+        })
+    }
+};
